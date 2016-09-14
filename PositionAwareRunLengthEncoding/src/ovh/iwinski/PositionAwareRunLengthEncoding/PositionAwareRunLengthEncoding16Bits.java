50a51c6afb083dad5a0083170a971973fa48cf7b
Iwan91 <kamil3453@yahoo.com>
2016-09-14 02:59:05 +0200

    added binary search for getValueAt and setValueAt

--- PositionAwareRunLengthEncoding/src/ovh/iwinski/PositionAwareRunLengthEncoding/PositionAwareRunLengthEncoding16Bits.java
+++ PositionAwareRunLengthEncoding/src/ovh/iwinski/PositionAwareRunLengthEncoding/PositionAwareRunLengthEncoding16Bits.java
@@ -78,11 +78,48 @@ public class PositionAwareRunLengthEncoding16Bits {
 		return result.toCharArray();
 	}
 
-	//Binary search probably can speed up searching for value.
+	private static int binarySearchLeft(char[] parleValues, char index){
+		char low = 0;
+		char high = (char) ((parleValues.length - 1)/3);
+		char mid;
+		while (low <= high) {
+			mid = (char) (low + (high - low) / 6);
+			
+			if(parleValues[mid*3+1]<=index && index<= parleValues[mid*3]-1+parleValues[mid*3+1])
+				return (char) (mid*3);
+			
+			if (parleValues[mid*3]-1+parleValues[mid*3+1] >= index)
+				high = (char) (mid - 1);
+			else
+				low = (char) (mid + 1);
+				
+		}
+		return -1;
+	}
+	
 	//this method implement naive algorithm for search
 	// return char as int. Return -1 when not found index
 	/**
-	 * Get value at index in sequence
+	 * Get value at index in sequence (naive algorithm)
+	 * @param  parleValues must be divisible by 3.<br>
+	 *         Max length of <strong>parleValues</strong> can be 196608.
+	 * @param  index get value at index
+	 * @return Return char as int<br>
+	 * 		   If found index then return char with value<br>
+	 * 		   else return -1<br>
+	 */
+	public static int getValueAtNaiveAlgorithm(char[] parleValues, char index){
+		for(char i=0;i<parleValues.length;i+=3){
+			if(parleValues[i]-1+parleValues[i+1]>=index){
+				return parleValues[i+2];
+			}
+		}
+		return -1;
+	}
+
+	// return char as int. Return -1 when not found index
+	/**
+	 * Get value at index in sequence (faster algorithm)
 	 * @param  parleValues must be divisible by 3.<br>
 	 *         Max length of <strong>parleValues</strong> can be 196608.
 	 * @param  index get value at index
@@ -91,15 +128,10 @@ public class PositionAwareRunLengthEncoding16Bits {
 	 * 		   else return -1<br>
 	 */
 	public static int getValueAt(char[] parleValues, char index) {	
-		for(char i=0;i<parleValues.length;i+=3){
-			if(parleValues[i]-1+parleValues[i+1]>=index){
-				return parleValues[i+2];
-			}
-		}
-		return -1;
+		return parleValues[binarySearchLeft(parleValues, index)+2];
 	}
 	
-	//Here probably we can faster change value if we use binary search instead for
+	
 	/**
 	 * Set value at index in sequence (faster algorithm)
 	 * @param  parleValues must be divisible by 3.<br>
@@ -111,108 +143,108 @@ public class PositionAwareRunLengthEncoding16Bits {
 	 */
 	public static char[] setValueAt(char[] parleValues, char index, char value){
 		char[] result = null;
-		//instead of below for there could be a binary search to optimize searching
-		for(char i=0;i<parleValues.length;i+=3){
-			if(parleValues[i]-1+parleValues[i+1]>=index){
-				//char[] d=decode(new char[]{parleValues[i],parleValues[i+1],parleValues[i+2]});
-				//the modified group being only a single length
-				if(parleValues[i]==1){
-					//left and right group
-					if((i-1>0 && parleValues[i-1]==value) && (i+5<parleValues.length && parleValues[i+5]==value)){
-						result=new char[parleValues.length-6];
+		int i=binarySearchLeft(parleValues, index);
+		if(i==-1) return null;
+
+		if(parleValues[i]-1+parleValues[i+1]>=index){
+			//char[] d=decode(new char[]{parleValues[i],parleValues[i+1],parleValues[i+2]});
+			//the modified group being only a single length
+			if(parleValues[i]==1){
+				//left and right group
+				if((i-1>0 && parleValues[i-1]==value) && (i+5<parleValues.length && parleValues[i+5]==value)){
+					result=new char[parleValues.length-6];
+					System.arraycopy(parleValues, 0, result, 0, i);
+					System.arraycopy(parleValues, i+6, result, i, parleValues.length-i-6);
+					result[i-3]+=parleValues[i+3]+1;
+				}
+				else{
+					// left group
+					if(i-1>0 && parleValues[i-1]==value){
+						result=new char[parleValues.length-3];
 						System.arraycopy(parleValues, 0, result, 0, i);
-						System.arraycopy(parleValues, i+6, result, i, parleValues.length-i-6);
-						result[i-3]+=parleValues[i+3]+1;
+						System.arraycopy(parleValues, i+3, result, i, parleValues.length-i-3);
+						result[i-3]+=1;
 					}
 					else{
-						// left group
-						if(i-1>0 && parleValues[i-1]==value){
+						//right group
+						if(i+5<parleValues.length && parleValues[i+5]==value){
 							result=new char[parleValues.length-3];
 							System.arraycopy(parleValues, 0, result, 0, i);
 							System.arraycopy(parleValues, i+3, result, i, parleValues.length-i-3);
-							result[i-3]+=1;
+							result[i]+=1;
+							result[i+1]-=1;
 						}
 						else{
-							//right group
-							if(i+5<parleValues.length && parleValues[i+5]==value){
-								result=new char[parleValues.length-3];
-								System.arraycopy(parleValues, 0, result, 0, i);
-								System.arraycopy(parleValues, i+3, result, i, parleValues.length-i-3);
-								result[i]+=1;
-								result[i+1]-=1;
-							}
-							else{
-								//no left and right group
-								result=new char[parleValues.length];
-								System.arraycopy(parleValues, 0, result, 0, parleValues.length);
-								result[i+2]=value;
-							}
+							//no left and right group
+							result=new char[parleValues.length];
+							System.arraycopy(parleValues, 0, result, 0, parleValues.length);
+							result[i+2]=value;
 						}
+					}
 
+				}
+			}
+			//being at the start of the modified group
+			else{
+				char[] newValue;
+				char position=(char) (index-parleValues[i+1]);
+				if(position==0){
+					//need check left group if the same!!!
+					if(i-1>0 && parleValues[i-1]==value){
+						result=new char[parleValues.length];
+						System.arraycopy(parleValues, 0, result, 0, parleValues.length);
+						result[i]-=1;
+						result[i+1]+=1;
+						result[i-3]+=1;
+					}
+					else{
+						newValue=encodeWithOffset(new char[]{value},index);
+						result=new char[parleValues.length+3];
+						System.arraycopy(parleValues, 0, result, 0, i);
+						System.arraycopy(newValue, 0, result, i, newValue.length);
+						System.arraycopy(parleValues, i, result, i+3, parleValues.length-i );
+						result[i+3]-=1;				
+						result[i+4]+=1;					
 					}
 				}
-				//being at the start of the modified group
+				//being at the end of the modified group
 				else{
-					char[] newValue;
-					char position=(char) (index-parleValues[i+1]);
-					if(position==0){
-						//need check left group if the same!!!
-						if(i-1>0 && parleValues[i-1]==value){
+					if(parleValues[i]==position+1){
+						//need check right group if the same!!!
+						if(i+5<parleValues.length && parleValues[i+5]==value){
 							result=new char[parleValues.length];
 							System.arraycopy(parleValues, 0, result, 0, parleValues.length);
 							result[i]-=1;
-							result[i+1]+=1;
-							result[i-3]+=1;
+							result[i+3]+=1;
+							result[i+4]-=1;
 						}
 						else{
 							newValue=encodeWithOffset(new char[]{value},index);
 							result=new char[parleValues.length+3];
-							System.arraycopy(parleValues, 0, result, 0, i);
-							System.arraycopy(newValue, 0, result, i, newValue.length);
-							System.arraycopy(parleValues, i, result, i+3, parleValues.length-i );
-							result[i+3]-=1;				
-							result[i+4]+=1;					
-						}
-					}
-					//being at the end of the modified group
-					else{
-						if(parleValues[i]==position+1){
-							//need check right group if the same!!!
-							if(i+5<parleValues.length && parleValues[i+5]==value){
-								result=new char[parleValues.length];
-								System.arraycopy(parleValues, 0, result, 0, parleValues.length);
-								result[i]-=1;
-								result[i+3]+=1;
-								result[i+4]-=1;
-							}
-							else{
-								newValue=encodeWithOffset(new char[]{value},index);
-								result=new char[parleValues.length+3];
-								System.arraycopy(parleValues, 0, result, 0, i+3);
-								System.arraycopy(newValue, 0, result, i+3, newValue.length);
-								System.arraycopy(parleValues, i+3, result, i+6, parleValues.length-i-3);
-								result[i]-=1;
-							}
-						}
-						else{
-							//being in the middle of the modified group
-							newValue=encodeWithOffset(new char[]{value},index);
-							result=new char[parleValues.length+6];
 							System.arraycopy(parleValues, 0, result, 0, i+3);
 							System.arraycopy(newValue, 0, result, i+3, newValue.length);
-							System.arraycopy(parleValues, i, result, i+6,parleValues.length-i);
-							result[i]=position;
-							result[i+6]=(char) (parleValues[i]-(position+1));
-							result[i+7]+=position+1;
+							System.arraycopy(parleValues, i+3, result, i+6, parleValues.length-i-3);
+							result[i]-=1;
 						}
 					}
-					
+					else{
+						//being in the middle of the modified group
+						newValue=encodeWithOffset(new char[]{value},index);
+						result=new char[parleValues.length+6];
+						System.arraycopy(parleValues, 0, result, 0, i+3);
+						System.arraycopy(newValue, 0, result, i+3, newValue.length);
+						System.arraycopy(parleValues, i, result, i+6,parleValues.length-i);
+						result[i]=position;
+						result[i+6]=(char) (parleValues[i]-(position+1));
+						result[i+7]+=position+1;
+					}
 				}
 
-				break;
 			}
+
 		}
-		
+
+
 		return result;
 	}
 	
@@ -223,6 +255,7 @@ public class PositionAwareRunLengthEncoding16Bits {
 	 * @param  index set value at index
 	 * @param  value to set
 	 * @return Return new encoded char[] with changed value at index.<br>
+	 * 		   If something go wrong throw exception java.lang.ArrayIndexOutOfBoundsException
 	 */
 	public static char[] setValueAtNaiveAlgorithm(char[] parleValues, char index, char value){
 		char[] tmp=decode(parleValues);
